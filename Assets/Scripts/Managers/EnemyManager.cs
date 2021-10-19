2e8a78aaaa912cf8466275f14252e05054d2a54e
Fathursyafeei <mufatraba@gmail.com>
2021-10-20 01:16:20 +0700

    add Script  EnemyManager

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -7,22 +7,49 @@ public class EnemyManager : MonoBehaviour
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
-
-    void Start ()
+    private void Start()
     {
+        // Mengeksekusi fungsi Spawn setiap beberapa detik sesuai nilai spawnTime
         InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
-
-    void Spawn ()
+    void Spawn()
     {
-        if (playerHealth.currentHealth <= 0f)
+        // Jika player telah mati maka tidak membuat enemy baru.
+        if(playerHealth.currentHealth <= 0f)
         {
             return;
         }
 
-        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        // Get nilai Random
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
+        // Menduplikasi enemy
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
 }
+
+
+//public PlayerHealth playerHealth;
+//public GameObject enemy;
+//public float spawnTime = 3f;
+//public Transform[] spawnPoints;
+
+
+//void Start ()
+//{
+//    InvokeRepeating("Spawn", spawnTime, spawnTime);
+//}
+
+
+//void Spawn ()
+//{
+//    if (playerHealth.currentHealth <= 0f)
+//    {
+//        return;
+//    }
+
+//    int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+//    Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+//}
